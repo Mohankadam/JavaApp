bcdb0c48cf7daf27527d1896f8fda89799363470
Mohankadam <mohankadam0@gmail.com>
2024-01-17 10:25:12 +0530

    Initial commit

--- src/com/JavaApp/SumTwoNumbers.java
+++ src/com/JavaApp/SumTwoNumbers.java
@@ -1,5 +1,6 @@
 package com.JavaApp;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class SumTwoNumbers {
@@ -17,6 +18,9 @@ public class SumTwoNumbers {
 		num1=num1+num2;
 		
 		System.out.println("Sum is :"+ num1);
+		
+		//Date
+		System.out.println("Jenkins Test Started:" + new Date());
 
 	}
 
